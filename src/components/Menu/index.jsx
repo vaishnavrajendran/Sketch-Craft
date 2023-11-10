0faf559d5822b4986d03a235ed209286a80f3ea7
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-11-10 13:20:32 +0530

    implemented redux-toolkit and menu tool box comm

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -7,25 +7,49 @@ import {
   faRotateRight,
   faFileArrowDown,
 } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import cx from "classnames";
 
 import styles from "./index.module.css";
+import { MENU_ITEMS } from "@/constants";
+import { menuItemClick } from "@/slice/menuSlice";
 
 const Menu = () => {
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const dispatch = useDispatch();
+  const hanldeMenuClick = (itemName) => {
+    dispatch(menuItemClick(itemName));
+  };
+
   return (
     <div>
       <div className={styles.menuContainer}>
-        <div className={styles.iconWrapper}>
+        <div
+          className={cx(styles.iconWrapper, {
+            [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL,
+          })}
+          onClick={() => hanldeMenuClick(MENU_ITEMS.PENCIL)}
+        >
           <FontAwesomeIcon icon={faPencil} className={styles.icon} />
         </div>
-        <div className={styles.iconWrapper}>
+
+        <div
+          className={cx(styles.iconWrapper, {
+            [styles.active]: activeMenuItem === MENU_ITEMS.ERASER,
+          })}
+          onClick={() => hanldeMenuClick(MENU_ITEMS.ERASER)}
+        >
           <FontAwesomeIcon icon={faEraser} className={styles.icon} />
         </div>
+
         <div className={styles.iconWrapper}>
           <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
         </div>
+
         <div className={styles.iconWrapper}>
           <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
         </div>
+
         <div className={styles.iconWrapper}>
           <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
         </div>
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import MenuReducer from "@/slice/menuSlice";
+
+export const store = configureStore({
+  reducer: {
+    menu: MenuReducer,
+  },
+});
